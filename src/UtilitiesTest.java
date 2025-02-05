c18ed8a15f1251cb9ce15ce730e49730dafa2fa5
Jake Dusing <dusingjake@gmail.com>
2025-02-04 18:49:45 -0600

    initial commit, add utitlies and utilitiestest classes

---
+++ src/UtilitiesTest.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.*;
+
+public class UtilitiesTest {
+
+    @org.junit.Test
+    public void everyNthChar() {
+        fail("This test has not been implemented");
+    }
+
+    @org.junit.Test
+    public void removePairs() {
+        Utilities util = new Utilities();
+        assertEquals("ABCDEF", util.removePairs("AABCDDEFF"));
+        assertEquals("ABCABDEF", util.removePairs("ABCCABDEEF"));
+    }
+
+    @org.junit.Test
+    public void converter() {
+        fail("This test has not been implemented");
+    }
+
+    @org.junit.Test
+    public void nullIfOddLength() {
+        fail("This test has not been implemented");
+    }
+}
